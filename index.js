aa9b7eef34885d2b2d6ce8d317e70899fae58824
Celiel Castillo <celiel13@gmail.com>
2023-06-19 13:51:39 -0400

    fixed pandemonioium and I can live for a couple more years

--- index.js
+++ index.js
@@ -14,49 +14,30 @@ function average(array) {
       const testsInput = document.getElementById('tests');
       const tests = parseInt(testsInput.value);
   
-      if (isNaN(tests)) 
-      {
+      if (isNaN(tests)) {
         resultDiv.textContent = 'Please enter a valid number of tests.';
         return;
       }
   
       const scores = [];
   
-      function getScore(index) 
-      {
+      function getScore(index) {
         const score = prompt(`Enter score for Test ${index + 1}:`);
         const parsedScore = parseInt(score);
-      
-        if (!isNaN(parsedScore)) 
-        {
+  
+        if (!isNaN(parsedScore)) {
           scores.push(parsedScore);
         }
-      
-        if (index < tests - 1) 
-        {
+  
+        if (index < tests - 1) {
           getScore(index + 1);
         } else {
           const avg = average(scores);
-          if (avg <= 59) 
-          {
-            resultDiv.textContent = `The grade is: F ${avg.toFixed(2)}`;
-          } else if (avg >= 60 && avg < 70) 
-          {
-            resultDiv.textContent = `The grade is: D ${avg.toFixed(2)}`;
-          } else if (avg >= 70 && avg < 80) 
-          {
-            resultDiv.textContent = `The grade is: C ${avg.toFixed(2)}`;
-          } else if (avg >= 80 && avg < 90) 
-          {
-            resultDiv.textContent = `The grade is: B ${avg.toFixed(2)}`;
-          } else if (avg >= 90 && avg <= 100) 
-          {
-            resultDiv.textContent = `The grade is: A ${avg.toFixed(2)}`;
-          }
+          resultDiv.textContent = `The average score is: ${avg.toFixed(2)}`;
         }
       }
-      
+  
       getScore(0);
-      
     });
   });
+  
