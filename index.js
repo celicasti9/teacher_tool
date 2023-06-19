b0b7e2cb24cbb30eaa5807ddf792130e466b7f91
Celiel Castillo <celiel13@gmail.com>
2023-06-19 14:00:28 -0400

    I died a little but I recovered the files

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
 function average(array) {
-    let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-      sum += array[i];
-    }
-    return sum / array.length;
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
   }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    const submitBtn = document.getElementById('submitBtn');
-    const resultDiv = document.getElementById('result');
-  
-    submitBtn.addEventListener('click', function() {
-      const testsInput = document.getElementById('tests');
-      const tests = parseInt(testsInput.value);
-  
-      if (isNaN(tests)) {
-        resultDiv.textContent = 'Please enter a valid number of tests.';
-        return;
+  return sum / array.length;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const submitBtn = document.getElementById('submitBtn');
+  const resultDiv = document.getElementById('result');
+
+  submitBtn.addEventListener('click', function() {
+    const testsInput = document.getElementById('tests');
+    const tests = parseInt(testsInput.value);
+
+    if (isNaN(tests)) {
+      resultDiv.textContent = 'Please enter a valid number of tests.';
+      return;
+    }
+
+    const scores = [];
+
+    function getScore(index) {
+      const score = prompt(`Enter score for Test ${index + 1}:`);
+      const parsedScore = parseInt(score);
+
+      if (!isNaN(parsedScore)) {
+        scores.push(parsedScore);
       }
-  
-      const scores = [];
-  
-      function getScore(index) {
-        const score = prompt(`Enter score for Test ${index + 1}:`);
-        const parsedScore = parseInt(score);
-  
-        if (!isNaN(parsedScore)) {
-          scores.push(parsedScore);
-        }
-  
-        if (index < tests - 1) {
-          getScore(index + 1);
-        } else {
-          const avg = average(scores);
-          resultDiv.textContent = `The average score is: ${avg.toFixed(2)}`;
-        }
+
+      if (index < tests - 1) {
+        getScore(index + 1);
+      } else {
+        const avg = average(scores);
+        resultDiv.textContent = `The average score is: ${avg.toFixed(2)}`;
       }
-  
-      getScore(0);
-    });
+    }
+
+    getScore(0);
   });
-  
+});
